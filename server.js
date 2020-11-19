087cb6dfe773cd57b860a07cbe898e2e1375d0ef
TiagoTR <tiagorocha.tr16@gmail.com>
2020-11-19 20:57:52 -0300

    Criação de uma rota, modal e controller referentes a essa rota

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//iniciando o app
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+//inciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', 
+    { useUnifiedTopology: true, useNewUrlParser: true }
+);
+requireDir('./src/models');
+
+//rotas
+app.use('/api', require("./src/routes"));
 
 app.listen(3001);
